7ffb2edb02bc212931f92db16ee3068c35018433
Fredy Abel Huanca Torres <abel.huanca@upeu.edu.pe>
2020-11-21 11:42:43 -0500

    Formularios React

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,3 +1,4 @@
+import AddUser from "./components/AddUser";
 import UsersList from "./components/UsersList";
 
 import React, { Component } from "react";
@@ -9,7 +10,11 @@ class App extends Component {
     super();
     this.state = {
       users: [],
+      username: "", // nuevo
+      email: "", // nuevo
     };
+    this.addUser = this.addUser.bind(this); // nuevo
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -27,16 +32,51 @@ class App extends Component {
       });
   }
 
+  addUser(event) {
+    event.preventDefault();
+    // nuevo
+    const data = {
+      username: this.state.username,
+      email: this.state.email,
+    };
+    // nuevo
+    axios
+      .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then((res) => {
+        this.getUsers(); // nuevo
+        this.setState({ username: "", email: "" }); // nuevo
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  }
+
   render() {
     return (
       <section className="section">
         <div className="container">
           <div className="columns">
-            <div className="column is-one-third">
+            <div className="column is-half">
+              {" "}
+              {/* new */}
               <br />
               <h1 className="title is-1">All Users</h1>
               <hr />
               <br />
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />
+              <br />
+              <br /> {/* nuevo */}
               <UsersList users={this.state.users} />
             </div>
           </div>
